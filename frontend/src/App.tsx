a62ad732eeeaca9f909d39b686d07e99dd55e3ec
Tyler Gurth <tyler_gurth@brown.edu>
2022-11-22 14:40:11 -0500

    added google auth

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { GoogleOAuthProvider } from '@react-oauth/google';
+import { GoogleLogin } from '@react-oauth/google';
+import jwt_decode from "jwt-decode";
+
+
 
 function App() {
+
+  const [fillName, setName] = useState()
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Edit <code>{fillName}</code> and save to reload.
         </p>
         <a
           className="App-link"
@@ -19,6 +26,24 @@ function App() {
           Learn React
         </a>
       </header>
+      <GoogleOAuthProvider clientId="359674566307-dej7oadld3jb6662l9brborqrs19blm4.apps.googleusercontent.com">
+        <GoogleLogin
+          onSuccess={credentialResponse => {
+            console.log(credentialResponse);
+            if (credentialResponse.credential != null) {
+              let decoded: any = jwt_decode(credentialResponse.credential);
+              console.log(decoded)
+              setName(decoded.name)
+            }
+
+          }}
+          onError={() => {
+            console.log('Login Failed');
+          }}
+        />;
+
+      </GoogleOAuthProvider>;
+
     </div>
   );
 }
