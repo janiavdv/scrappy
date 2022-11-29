6d76934277e0cd890be16dbd6096326ff77490ff
janiavdv <jania_vandevoorde@brown.edu>
2022-11-29 13:10:15 -0500

    add logic for checking if user account exists

--- frontend/src/pages/profile.tsx
+++ frontend/src/pages/profile.tsx
@@ -1,8 +1,22 @@
 import { useLocation } from "react-router-dom";
 
+const list_of_users: Array<string> = ["jania_vandevoorde@brown.edu"]
+
+function LogModal() {
+    return (
+        <p> {"Popup window"} </p>
+    )
+}
+
 export default function Profile() {
     const st = useLocation().state
-    return (
-        <h1>{st.name}</h1>
-    )
+    if (list_of_users.includes(st.email)) {
+        return (
+            <p>{st.name + " has an account."}</p>
+        )
+    } else {
+        return (
+            <h1>{st.name + " does not have an account yet."}</h1>
+        )
+    }
 }
