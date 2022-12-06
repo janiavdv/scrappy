b575525936864919936d15814f72b73132046d30
Alana <alana_cho@brown.edu>
2022-12-06 10:43:28 -0500

    updating mongodb link

--- backend/src/main/java/database/MongoDB.java
+++ backend/src/main/java/database/MongoDB.java
@@ -1,9 +1,11 @@
 package database;
 
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 import java.net.UnknownHostException;
-import com.mongodb.MongoClient;
+import java.util.ArrayList;
 import org.bson.Document;
 
 public class MongoDB {
@@ -13,20 +15,21 @@ public class MongoDB {
   private static MongoCollection<Document> usersColl;
 
   public MongoDB() {
-    client = new MongoClient("localhost", 27017);
+    client = MongoClients.create("mongodb+srv://acho28:scrappy@scrappy.uwn0mgi.mongodb.net/test");
     database = client.getDatabase("myDatabase");
+    System.out.println(database);
     usersColl = database.getCollection("Users");
 
-//    System.out.println(coll);
+//    System.out.println(usersColl);
 //
 //    Document user = new Document("_id", "tgurth");
 //    user.append("email", "tyler_gurth@brown.edu");
 //    user.append("books", new ArrayList<>())
 //        .append("entries", new ArrayList<>())
 //            .append("friends", new ArrayList<>());
-//    coll.insertOne(user);
-//
-//    Document doc = coll.find(new Document("_id", "tgurth")).first();
+//    usersColl.insertOne(user);
+
+//    Document doc = usersColl.find(new Document("_id", "tgurth")).first();
 //    System.out.println(doc.toJson());
   }
 
