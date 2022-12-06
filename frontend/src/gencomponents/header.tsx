95d2e24936f1f8c77dc27b41eefe801d54f0ec80
janiavdv <jania_vandevoorde@brown.edu>
2022-12-06 16:29:17 -0500

    update buttons to navigate to endpoints

--- frontend/src/gencomponents/header.tsx
+++ frontend/src/gencomponents/header.tsx
@@ -1,24 +1,31 @@
 import User from './user';
+import { useNavigate } from "react-router-dom";
 
 export default function Header({user}: {user: User}) {
+
+    const navigate = useNavigate();
+
     return (
         <div id="header">
             <div>
                 <button 
                 onClick={async () => {
                     console.log("Friends clicked!")
+                    navigate("/friends")
                 }}
                 id="headerButton">Friends</button>
                 
                 <button 
                 onClick={async () => {
                     console.log("Gallery clicked!")
+                    navigate("/gallery")
                 }}
                 id="headerButton">Gallery</button>
 
                 <button 
                 onClick={async () => {
                     console.log("Profile clicked!")
+                    navigate("/profile")
                 }}
                 id="headerButton">Profile</button>
 
