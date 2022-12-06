dd3e1ac239a7b9985c43513bb017f082ad57eb31
janiavdv <jania_vandevoorde@brown.edu>
2022-12-06 16:23:16 -0500

    update header to have buttons

--- frontend/src/gencomponents/header.tsx
+++ frontend/src/gencomponents/header.tsx
@@ -4,9 +4,24 @@ export default function Header({user}: {user: User}) {
     return (
         <div id="header">
             <div>
-                <span>Friends</span>
-                <span>Gallery</span>
-                <span>Profile</span>
+                <button 
+                onClick={async () => {
+                    console.log("Friends clicked!")
+                }}
+                id="headerButton">Friends</button>
+                
+                <button 
+                onClick={async () => {
+                    console.log("Gallery clicked!")
+                }}
+                id="headerButton">Gallery</button>
+
+                <button 
+                onClick={async () => {
+                    console.log("Profile clicked!")
+                }}
+                id="headerButton">Profile</button>
+
                 <img src={user.picture} id="logopic" />
             </div>
         </div>
