df06c722f519ccf1aad181ba8b90ecb683017c27
Tyler Gurth <tyler_gurth@brown.edu>
2022-12-07 12:24:09 -0500

    finished AWS

--- frontend/src/gencomponents/awsupload.tsx
+++ frontend/src/gencomponents/awsupload.tsx
@@ -1,14 +1,15 @@
-import React , {useState} from 'react';
+import React, { useState } from 'react';
 import { uploadFile } from 'react-s3'
-
+// https://stackoverflow.com/questions/69686231/react-s3-error-referenceerror-buffer-is-not-defined
+window.Buffer = window.Buffer || require("buffer").Buffer;
 
 // https://docs.aws.amazon.com/sdk-for-javascript/v2/developer-guide/s3-example-photo-album.html
 // https://javascript.plainenglish.io/how-to-upload-files-to-aws-s3-in-react-591e533d615e
 
-const S3_BUCKET ='scrappyuploads';
-const REGION ='us-east-1';
-const ACCESS_KEY ='AKIAVSEGDI5QPF7BRARY';
-const SECRET_ACCESS_KEY ='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';
+const S3_BUCKET = 'scrappyuploads';
+const REGION = 'us-east-1';
+const ACCESS_KEY = 'AKIAVSEGDI5QPF7BRARY';
+const SECRET_ACCESS_KEY = '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';
 
 const config = {
     bucketName: S3_BUCKET,
@@ -21,21 +22,23 @@ const UploadImageToS3WithReactS3 = () => {
 
     const [selectedFile, setSelectedFile] = useState(null);
 
-    const handleFileInput = (e : any) => {
+    const handleFileInput = (e: any) => {
         setSelectedFile(e.target.files[0]);
     }
 
-    const handleUpload = async (file : any) => {
+    const handleUpload = async (file: any) => {
         uploadFile(file, config)
             .then((data: any) => console.log(data))
             .catch((err: any) => console.error(err))
     }
 
-    return <div>
-        <div>React S3 File Upload</div>
-        <input type="file" onChange={handleFileInput}/>
-        <button onClick={() => handleUpload(selectedFile)}> Upload to S3</button>
-    </div>
+    return (
+        <div>
+            <div>React S3 File Upload</div>
+            <input type="file" onChange={handleFileInput} />
+            <button onClick={() => handleUpload(selectedFile)}> Upload to S3</button>
+        </div>
+    )
 }
 
 export default UploadImageToS3WithReactS3;
--- frontend/src/pages/profile.tsx
+++ frontend/src/pages/profile.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { useLocation } from "react-router-dom";
 import User  from "../gencomponents/user";
 import Header from '../gencomponents/header';
+import UploadImageToS3WithReactS3 from "../gencomponents/awsupload";
 
 
 export const TEXT_text_box_accessible_name = "Text Box for Information Entry.";
@@ -22,6 +23,9 @@ export default function Profile() {
             <p>{user.email}</p>
             <p>{user.username}</p>
             <img src={user.picture} id="logopic" />
+
+            <UploadImageToS3WithReactS3 />
         </div>
+
     )
 }
