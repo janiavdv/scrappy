dce931ca59d9d050944ebfdb1e7c824afe5f3e14
janiavdv <jania_vandevoorde@brown.edu>
2022-12-08 14:24:57 -0500

    feat: footer buttons

--- frontend/src/gencomponents/footer.tsx
+++ frontend/src/gencomponents/footer.tsx
@@ -1,13 +1,43 @@
+import User from './user';
+import { useNavigate } from "react-router-dom";
+import { useMemo, useState } from 'react';
 
 export default function Footer() {
+
+    const navigate = useNavigate();
+    const [userValue, setUserValue] = useState<string>("") // For controlling the user textbox.
+
     return (
         <div id="footer">
             <div>
-                <span>Help</span>
-                <span>About</span>
+                <button
+                        onClick={async () => {
+                            console.log("About clicked!")
+                            navigate("/about")
+                        }}
+                        id="footerButton">About</button>
+                <button
+                        onClick={async () => {
+                            console.log("Help clicked!")
+                            navigate("/help")
+                        }}
+                        id="footerButton">Help</button>
+                
                 <h3>Scrappy © All Rights Reserved</h3>
-                <span>FAQs</span>
-                <span>Privacy</span>
+                
+                <button
+                        onClick={async () => {
+                            console.log("FAQs clicked!")
+                            navigate("/faqs")
+                        }}
+                        id="footerButton">FAQs</button>
+
+                <button
+                        onClick={async () => {
+                            console.log("Privacy clicked!")
+                            navigate("/privacy")
+                        }}
+                        id="footerButton">Privacy</button>
             </div>
         </div>
     )
