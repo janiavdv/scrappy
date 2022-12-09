becf792b9f3ca0a29e052f12c220995fe5c4a920
janiavdv <jania_vandevoorde@brown.edu>
2022-12-09 13:37:12 -0500

    begin book history (has a bug)

--- frontend/src/pages/profile.tsx
+++ frontend/src/pages/profile.tsx
@@ -1,35 +1,56 @@
-import { useState } from "react";
-import { useLocation } from "react-router-dom";
+import { Dispatch, SetStateAction, useState } from 'react';import { useLocation } from "react-router-dom";
 import User from "../gencomponents/user";
 import Header from '../gencomponents/header';
 import UploadImageToS3WithReactS3 from "../gencomponents/awsupload";
 import ControlledInput from '../gencomponents/controlledinput';
 import Footer from "../gencomponents/footer";
 import { useNavigate } from "react-router-dom";
+import { setDefaultResultOrder } from "dns";
+// import Page from '../bookelements/page'
 
 
-// interface Book {
-//     title: string,
-//     pages: [Page]
-// }
-
-interface Page {
+interface PageProps {
     title: string,
     body: string
 }
 
-interface ModalProps {
-    pageTitle: string,
-    pageBody: string,
-    display: boolean
+// The HTML for a Page.
+function Page({title, body}: PageProps) {
+    return (
+        <div className="page">
+          <code>Title: {title}</code>
+          <br />
+          <code>Body: {body}</code>
+        </div>
+      );
 }
 
-function LogModal({ pageTitle, pageBody, display }: ModalProps) {
+interface BookProps {
+    pages: PageProps[],
+    setPages: Dispatch<SetStateAction<PageProps[]>>
+}
+
+// The HTML for a Book.
+function Book({pages, setPages}: BookProps) {
+    return (
+        <div className="book">
+            {pages.map((page) => (
+                <Page title={page.title} body={page.body} />
+            ))}
+        </div>
+    );
+}
+
+interface ModalProps {
+    display: boolean
+    setDisplay: Dispatch<SetStateAction<boolean>>
+}
+
+function LogModal({ display, setDisplay }: ModalProps, { pages, setPages }: BookProps) {
     const [titleValue, setTitleValue] = useState<string>("") // For controlling the title textbox.
     const [bodyValue, setBodyValue] = useState<string>("") // For controlling the body textbox.
-
+    
     const st: User = useLocation().state
-    const navigate = useNavigate();
 
     if (!display) { return null; } 
     
@@ -50,13 +71,13 @@ function LogModal({ pageTitle, pageBody, display }: ModalProps) {
                 <hr></hr>
                 <button type="submit" value="Post" onClick={() => {
                     if (titleValue != "" && bodyValue != "") {
-                        const pg: Page = {
+                        const pg: PageProps = {
                             title: titleValue,
                             body: bodyValue
                         }
-                        // addUserToDatabase(user); (TODO: add to history)
-                        // navigate("/profile:" + st.username, { state: st })
-                        navigate("/friends", { state: st }) // TODO: change this
+                        setPages([...pages, pg])
+                        setDisplay(false)
+                        
                     }
                 }} >Post</button>
             </div>
@@ -77,7 +98,7 @@ export default function Profile() {
         taglist: st.taglist
     })
     const [modalDisplay, setModalDisplay] = useState<boolean>(false) // For controlling the user textbox.
-
+    const [pages, setPages] = useState<PageProps[]>([])
 
     return (
         <div>
@@ -107,7 +128,7 @@ export default function Profile() {
                     </div>
                     <hr></hr>
                     <div id="today-book">
-                        <p>
+                        {/* <p>
                             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quam pellentesque nec nam aliquam. Vitae justo eget magna fermentum iaculis eu non diam phasellus. Vitae justo eget magna fermentum iaculis eu non diam. Sed arcu non odio euismod lacinia at. Faucibus scelerisque eleifend donec pretium vulputate sapien nec sagittis aliquam. Cras fermentum odio eu feugiat pretium nibh ipsum. Scelerisque mauris pellentesque pulvinar pellentesque habitant morbi tristique. Morbi tincidunt ornare massa eget egestas purus viverra accumsan in. Eget aliquet nibh praesent tristique magna sit. In ornare quam viverra orci sagittis eu volutpat odio facilisis. Fringilla est ullamcorper eget nulla facilisi etiam dignissim diam quis.
 
                             Velit ut tortor pretium viverra suspendisse potenti. Praesent elementum facilisis leo vel fringilla. Rhoncus urna neque viverra justo nec ultrices dui sapien eget. Ultrices eros in cursus turpis massa tincidunt dui ut ornare. Scelerisque viverra mauris in aliquam sem fringilla ut morbi. Quam viverra orci sagittis eu. Velit laoreet id donec ultrices tincidunt arcu non sodales. Libero volutpat sed cras ornare arcu dui. Non diam phasellus vestibulum lorem. Nulla at volutpat diam ut. Nibh venenatis cras sed felis eget velit aliquet. Risus quis varius quam quisque id. Tempus iaculis urna id volutpat lacus laoreet non. Massa sapien faucibus et molestie ac. Eu turpis egestas pretium aenean. Adipiscing tristique risus nec feugiat. Faucibus pulvinar elementum integer enim. Nibh cras pulvinar mattis nunc sed blandit libero volutpat.
@@ -119,7 +140,8 @@ export default function Profile() {
                             Vitae nunc sed velit dignissim sodales ut eu. Id interdum velit laoreet id donec. Augue ut lectus arcu bibendum at varius vel. Sodales ut eu sem integer vitae. Ornare quam viverra orci sagittis eu volutpat. Sit amet nulla facilisi morbi tempus iaculis urna. Amet nisl purus in mollis nunc sed. Dignissim enim sit amet venenatis urna cursus. Morbi tempus iaculis urna id volutpat lacus laoreet non. Vulputate odio ut enim blandit volutpat maecenas volutpat. Dui ut ornare lectus sit. Viverra accumsan in nisl nisi scelerisque eu ultrices vitae.
 
                             Ac orci phasellus egestas tellus rutrum tellus pellentesque. Consectetur adipiscing elit ut aliquam purus sit amet. Libero volutpat sed cras ornare arcu dui vivamus arcu. Nibh praesent tristique magna sit. Senectus et netus et malesuada fames ac turpis egestas integer. Risus viverra adipiscing at in. Quisque sagittis purus sit amet volutpat consequat mauris nunc congue. Adipiscing tristique risus nec feugiat in fermentum. Diam maecenas sed enim ut sem. Netus et malesuada fames ac turpis. Mauris augue neque gravida in fermentum et sollicitudin. Massa massa ultricies mi quis hendrerit. Imperdiet massa tincidunt nunc pulvinar sapien et ligula ullamcorper malesuada.
-                        </p>
+                        </p> */}
+                        <Book pages={pages} setPages={setPages} />
                     </div>
 
                 </div>
@@ -129,13 +151,10 @@ export default function Profile() {
                     <hr></hr>
                     </div>
                     <div id="newPageButton"> 
-                        <button type="submit" value="New Page" onClick={() => {
-                            setModalDisplay(true)
-                        }}
-                        >New Page</button>
+                        <button type="submit" value="New Page" onClick={() => { setModalDisplay(true) }}>New Page</button>
                     </div>
                 </div>
-                <LogModal pageTitle={""} pageBody={""} display={modalDisplay}></LogModal>
+                <LogModal display={modalDisplay} setDisplay={setModalDisplay} pages={pages} setPages={setPages}></LogModal>
             </div>
 
 
