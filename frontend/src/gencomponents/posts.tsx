3809a8efac05d758a84dfc2e494c25ea74194ac0
Alana <alana_cho@brown.edu>
2022-12-19 11:56:35 -0500

    aria labels for posts

--- frontend/src/gencomponents/posts.tsx
+++ frontend/src/gencomponents/posts.tsx
@@ -16,10 +16,16 @@ interface PostProps {
 
 function Post({ caption, title, tag, image, time, friend }: PostProps) {
   if (friend) {
+
+    const TEXT_friend_info = `This post was made by ${friend.username} at ${time}.`
+    const TEXT_image_description = `Here is the image.`
+    const TEXT_content_info = `The title of this post is ${title} and the caption 
+    reads ${caption}. The hashtag is ${tag}.`
+
     return (
       <div className="post">
         <div className="friend-post-top-bar">
-          <div className="friend-post-info">
+          <div className="friend-post-info" aria-label={TEXT_friend_info}>
             <img
               src={friend.image}
               className="friend-profile-pic"
@@ -30,12 +36,12 @@ function Post({ caption, title, tag, image, time, friend }: PostProps) {
           <p>{time}</p>
         </div>
         <hr></hr>
-        <div>
+        <div aria-label={TEXT_image_description}>
           <img className="post-image" src={image} />
         </div>
         <hr></hr>
         <div className="friend-post-bottom-bar">
-          <div className="post-content-info">
+          <div className="post-content-info" aria-label={TEXT_content_info}>
             <p>{title}</p>
             <p className="hashtag">{"#" + tag}</p>
           </div>
@@ -55,6 +61,8 @@ interface FriendPostProps {
 export default function FriendPosts({ friendList }: FriendPostProps) {
   const [friendPosts, setPosts] = useState<Entry[] | null>(null);
 
+  const TEXT_friend_posts = `Welcome to your friends feed!`
+
   useEffect(() => {
     if (friendList != null) {
       grabOrderedFriendPosts(friendList).then((lst) => setPosts(lst));
@@ -64,7 +72,7 @@ export default function FriendPosts({ friendList }: FriendPostProps) {
   }, []);
 
   return (
-    <div id="friend-post-section">
+    <div id="friend-post-section" aria-label={TEXT_friend_posts}>
       {friendPosts ? (
         friendPosts.map((post) => (
           <Post
@@ -97,15 +105,20 @@ interface GalleryPostProps {
 }
 
 function GalleryPost({ title, time, image, tag, username }: GalleryPostProps) {
+
+  const TEXT_gallery_content = `This post was made by ${username} at ${time}.`
+  const TEXT_post_info = `The title of this post is ${title} and the hashtag is
+  ${tag}.`
+
   return (
     <div className="gallery-post">
       <img className="gallery-image" src={image} />
       <div className="gallery-post-bottom-bar">
-        <div className="gallery-content-info">
+        <div className="gallery-content-info" aria-label={TEXT_gallery_content}>
           <h4>{username}</h4>
           <p>{time}</p>
         </div>
-        <div className="gallery-content-info">
+        <div className="gallery-content-info" aria-label={TEXT_post_info}>
           <p>{title}</p>
           <p className="hashtag">{"#" + tag}</p>
         </div>
@@ -127,8 +140,10 @@ export function GalleryPosts({ user }: GalleryProps) {
     });
   }, []);
 
+  const TEXT_gallery_page=`Welcome to the gallery page!`
+
   return (
-    <div id="gallery-page">
+    <div id="gallery-page" aria-label={TEXT_gallery_page}>
       <div id="gallery-info-box">
         <p>Your personal feed, catered to your interests.</p>
       </div>
