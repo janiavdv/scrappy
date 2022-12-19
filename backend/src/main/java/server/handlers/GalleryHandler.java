0384eafb7591e44afc154fe7ac20ea4de8662884
Tyler Gurth <tyler_gurth@brown.edu>
2022-12-19 09:18:44 -0500

    commented algorithm

--- backend/src/main/java/server/handlers/GalleryHandler.java
+++ backend/src/main/java/server/handlers/GalleryHandler.java
@@ -13,19 +13,33 @@ import spark.Route;
 import utils.ResponseUtil;
 import utils.VectorUtil;
 
+/**
+ * This is our DatabaseHandler class, which implements the Route interface.
+ * It handles the logic behind filling the gallery.
+ */
 public class GalleryHandler implements Route {
 
+  /**
+   * In the handle method we take in the parameters and return a list of Entry objects to the
+   * frontend in json format.
+   * @param request the associated parameters
+   * @param response
+   * @return a Json map containing a result and posts array
+   * @throws Exception
+   */
   @Override
   public Object handle(Request request, Response response) throws Exception {
     Map<String, Object> reply = new HashMap<>();
 
     try {
+      // We grab the tags of a user, as well as all the available public posts we *could* display.
       ArrayList<String> userTags = (ArrayList<String>) this.getUserTags(request.queryParams("username"));
       HashMap<String, ArrayList<Object>> tagToEntries = this.getComparableTags(request.queryParams("date"), request.queryParams("username"));
 
-//    // algorithm to calculate similarity score here//
+      // We use the algorithmic method generatePostList to create a list of Entries in order that
+      // we want to fill the gallery with.
       ArrayList<Object> orderedEntries = VectorUtil.generatePostList(userTags, tagToEntries);
-      System.out.println("Ordered Entries: " + orderedEntries);
+      // Putting it into a map.
       reply.put("result", "success");
       reply.put("posts", orderedEntries);
       return new ResponseUtil(reply).serialize();
@@ -36,33 +50,51 @@ public class GalleryHandler implements Route {
     }
   }
 
+  /**
+   * Helper method which grabs the tags (interests) of a given user.
+   * @param username the user for which to get the informaiton of.
+   * @return a list of their interests, strings.
+   */
   private List<String> getUserTags(String username) {
     try {
+      // Grabbing the user from the database.
       Document user = Server.getMyDatabase().getUsersColl().find(
           new Document("username", username)).first();
+      // Getting their list of tags.
       List<String> tags = user.getList("tags", String.class);
       return tags;
     } catch (NullPointerException e) {
-      return null; // need to write failure response
+      return new ArrayList<String>();
     }
   }
 
+  /**
+   * Helper method to get all the comparable posts, a map of each tag to the list of posts
+   * that share that tag (as they will be ordered together).
+   * @param date the date of posts we want
+   * @param username the username of the person, so we don't grab their posts.
+   * @return a HashMap of string to object, tag to list of Entry posts.
+   */
   private HashMap<String, ArrayList<Object>> getComparableTags(String date, String username) {
+    // We grab all the entries that match the current date.
     FindIterable<Document> dayEntries = Server.getMyDatabase().getEntriesColl()
         .find(new Document("date", date));
     List<Document> publicEntries = new ArrayList<>();
 
+    // We take all the entries that are 1. public and 2. not the logged in user's
     for (Document entry : dayEntries) {
       if (entry.get("publicized", Boolean.class).equals(true) && !entry.get("user", String.class).equals(username)) {
+        // Adding to our acceptable entries.
         publicEntries.add(entry);
       }
     }
 
     HashMap<String, ArrayList<Object>> tagToEntries = new HashMap<>();
 
+    // Associating the tag to each entry
     for (Document validEntry: publicEntries) {
       String tag = validEntry.get("tag").toString();
-      if (tagToEntries.containsKey(tag)) {
+      if (tagToEntries.containsKey(tag)) { // If it is already in the map
         tagToEntries.get(tag).add(validEntry);
       } else {
         ArrayList<Object> entryList = new ArrayList<>();
@@ -71,6 +103,7 @@ public class GalleryHandler implements Route {
       }
     }
 
+    // We return the hashamp of tag -> list of entries.
     return tagToEntries;
   }
 }
--- backend/src/main/java/utils/VectorUtil.java
+++ backend/src/main/java/utils/VectorUtil.java
@@ -8,10 +8,18 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 
+/**
+ * Class responsible for holding the vectorizing algorithms.
+ */
 public class VectorUtil {
 
   public static HashMap<String, ArrayList<Double>> wordEmbeddings;
 
+  /**
+   * Called upon server instantiation, works to generate the stored map of word to vector
+   * embeddings.
+   * @throws IOException
+   */
   public static void generateEmbeddings()
       throws IOException {
     String filePath = "src/data/glove.6b.50d.txt";
@@ -21,15 +29,16 @@ public class VectorUtil {
     String line = modelInput.readLine();
     long time = System.currentTimeMillis();
     double count = 0;
+    // We go through every line, separating the word and the list of double vectors.
     while (line != null) {
+      // Separating the values.
       ArrayList<String> brokenLine = new ArrayList<>(List.of(line.split(" ")));
-      String word = brokenLine.remove(0);
+      String word = brokenLine.remove(0); // Taking out the word.
       ArrayList<Double> vectors = new ArrayList<>();
       for (int i = 0; i < brokenLine.size(); i++)  {
-//        System.out.println(i);
-        vectors.add(Double.valueOf(brokenLine.get(i)));
+        vectors.add(Double.valueOf(brokenLine.get(i))); // creating a list of the vectors for word
       }
-      embeddings.put(word, vectors);
+      embeddings.put(word, vectors); // mapping it into our embedding map.
       count++;
       System.out.println(count / 400000);
       line = modelInput.readLine();
@@ -38,6 +47,14 @@ public class VectorUtil {
     wordEmbeddings = embeddings;
   }
 
+
+  /**
+   * Algorithm to take in all the user's tags and the posts that can be shown, and generates an orderedlist
+   * of the entries to show on the gallery page.
+   * @param tags the user's interests
+   * @param tagToEntries all available posts
+   * @return and ordered list of the posts.
+   */
   public static ArrayList<Object> generatePostList(ArrayList<String> tags, HashMap<String, ArrayList<Object>> tagToEntries) {
     ArrayList<Object> orderedList = new ArrayList<>();
     HashMap<String, ArrayList<Double>> stringToVector = new HashMap<>();
@@ -45,6 +62,7 @@ public class VectorUtil {
     HashMap<Double, String> scoreToString = new HashMap<>();
 
 
+    // We get the vector for each word, and if it isn't in the dataset, we just dont associate it.
     for (String t : tagToEntries.keySet()) {
       ArrayList<Double> tagVector = getVector(t);
 
@@ -57,8 +75,8 @@ public class VectorUtil {
       ArrayList<Double> tagVector = getVector(tag);
       if (tagVector != null) {
         for (String s: stringToVector.keySet()) {
+          // Getting the score similarity between each user tag, and the word associated with each post.
           Double score = cosineSimilarity(tagVector, stringToVector.get(s));
-//          System.out.println("Vector score: " + tag + " " + s + " :" + score.toString());
           if (stringToUnaverageredScore.containsKey(s)) {
             stringToUnaverageredScore.get(s).add(score);
           } else {
@@ -70,19 +88,19 @@ public class VectorUtil {
       }
     }
 
-//    System.out.println(stringToUnaverageredScore);
-
     for (String s : stringToUnaverageredScore.keySet()) {
 //      https://stackoverflow.com/questions/10791568/calculating-average-of-an-array-list
-
+      // We take the lists of scores and average them across all the tags.
       double averageScore = stringToUnaverageredScore.get(s).stream().mapToDouble(d -> d).average().orElse(0.0);
       scoreToString.put(averageScore, s);
     }
 
+    // We sort this based on how good a score is.
     ArrayList<Double> sortedScores = new ArrayList<>(scoreToString.keySet());
     Collections.sort(sortedScores);
     Collections.reverse(sortedScores);
 
+    // Push it through the list.
     for (Double score: sortedScores) {
       orderedList.add(tagToEntries.get(scoreToString.get(score)));
     }
@@ -90,20 +108,33 @@ public class VectorUtil {
   }
 
 
+  /**
+   * Method which provides an embedding vector for a given word, which we can then compare.
+   * @param word string to which we want a vector for
+   * @return the vector of that word
+   */
   public static ArrayList<Double> getVector(String word) {
     return (wordEmbeddings.get(word));
   }
 
+
+  /**
+   * Method which calculates the cosine similarity between two words (how similar they are).
+   * @param vectorA first word vector
+   * @param vectorB second word vector
+   * @return the similarity score between them.
+   */
   //https://stackoverflow.com/questions/520241/how-do-i-calculate-the-cosine-similarity-of-two-vectors
   public static Double cosineSimilarity(ArrayList<Double> vectorA, ArrayList<Double> vectorB) {
     double dotProduct = 0.0;
     double normA = 0.0;
     double normB = 0.0;
+    // Calculating the dot product over the roots of the normals.
     for (int i = 0; i < vectorA.size(); i++) {
       dotProduct += vectorA.get(i) * vectorB.get(i);
       normA += Math.pow(vectorA.get(i), 2);
       normB += Math.pow(vectorB.get(i), 2);
     }
-    return dotProduct / (Math.sqrt(normA) * Math.sqrt(normB));
+    return dotProduct / (Math.sqrt(normA) * Math.sqrt(normB)); // A score, 1 is a perfect match, 0 no similarity.
   }
 }
